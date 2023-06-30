822c264e5799e6cf710843fc4684f385445efe24
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-30 02:16:32 -0400

    Add POST to /api/persons

--- index.js
+++ index.js
@@ -21,6 +21,33 @@ app.get('/info', (request, response) => {
   );
 })
 
+const generateId = () => {
+  const maxId = phonebook.length > 0
+    ? Math.max(...phonebook.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  phonebook = phonebook.concat(person)
+
+  response.json(person)
+})
+
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
   const person = phonebook.find(person => person.id === id);
