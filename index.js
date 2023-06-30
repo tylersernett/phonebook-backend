a3156095e842e71f96facc7ec7a1307142f87499
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-30 14:27:02 -0400

    Deploy frontend

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postJSON'));
 morgan.token('postJSON', function (req, res) { return JSON.stringify(req.body) })
+app.use(express.static('dist'))
+
 
 const PORT = 3001
 app.listen(PORT)
