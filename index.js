fe780c2aaa2e0b971c170cbdde5c7ff1038fe85a
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-30 01:53:08 -0400

    Add GET for /info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //hour used: 1.5
 const express = require('express')
 const app = express()
-const db = require('./persons.json')
+const phonebook = require('./persons.json')
 
 const PORT = 3001
 app.listen(PORT)
@@ -10,5 +10,13 @@ console.log(`Server running on port ${PORT}`)
 app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
-  response.send(db)
+  response.send(phonebook)
+})
+
+app.get('/info', (request, response) => {
+  const timestamp = new Date().toString();
+  const entryCount = phonebook.length;
+  response.send(
+    `Phonebook has info for ${entryCount} people <br/> ${timestamp}`
+  );
 })
