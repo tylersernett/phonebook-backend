b5085bc903d5954c6ca8e40ba1d79450632f1097
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-30 22:34:33 -0400

    Convert DELETE to mongodb

--- index.js
+++ index.js
@@ -96,11 +96,12 @@ app.get('/api/persons/:id', (request, response, next) => {
 //   response.status(404).end();
 // }
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  phonebook = phonebook.filter(person => person.id !== id);
-  console.log(phonebook)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // app.put('/api/persons/:id', (request, response) => {
