d56409ced51f4137aac42700f7289e343158e6a9
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-30 22:45:29 -0400

    Delete old comments

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const express = require('express')
 const morgan = require('morgan')
 const Person = require('./models/person')
 const app = express()
-let phonebook = require('./persons.json')
 const person = require('./models/person')
 
 //MIDDLEWARE
@@ -15,12 +14,10 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postJSON'));
 morgan.token('postJSON', function (req, res) { return JSON.stringify(req.body) })
 
-
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(people => {
@@ -36,13 +33,6 @@ app.get('/info', (request, response) => {
     ))
 })
 
-// const generateId = () => {
-//   const maxId = phonebook.length > 0
-//     ? Math.max(...phonebook.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
-
 app.post('/api/persons', (request, response) => {
   const body = request.body
   console.log(body)
@@ -57,25 +47,14 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  // const existingPerson = phonebook.find(person => person.name === body.name)
-  // if (existingPerson) {
-  //   return response.status(400).json({
-  //     error: 'name must be unique'
-  //   })
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number,
-    // id: generateId(),
   })
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-
-  // phonebook = phonebook.concat(person)
-  // response.json(person)
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -89,12 +68,6 @@ app.get('/api/persons/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
-// const person = phonebook.find(person => person.id === id);
-// if (person) {
-//   response.send(person)
-// } else {
-//   response.status(404).end();
-// }
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
@@ -103,14 +76,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
-
-// app.put('/api/persons/:id', (request, response) => {
-//   console.log(request.body)
-//   const id = Number(request.params.id);
-//   const personToUpdate = phonebook.find(person => person.id === id);
-//   console.log(personToUpdate)
-//   response.status(204).end()
-// })
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
@@ -129,8 +94,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 
-//404 and 400 handling \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
+//ERROR handling \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
