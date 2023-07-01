966efd9c1cc8a6b177717c6d7ce5e7841e515b75
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-30 21:54:53 -0400

    Add CLI argv.length logic

--- mongo.js
+++ mongo.js
@@ -19,12 +19,23 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-  name: process.argv[3],
-  number: process.argv[4],
-})
+if (process.argv.length > 3) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
 
-person.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+  person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+
+} else if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log(`phonebook:`)
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
